d2e93ff692fce91fbcc4689598ad6e60b6b1ee31
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-04-08 10:43:14 +0600

    added bookmark and others things

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Content.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Content = (props) => {
-    const { img,authorImg,name,date,time} = props.content
+    const { img, authorImg, name, date, time,title} = props.content
     return (
         <div className='product-container-for-main'>
             <img src={img} alt="" />
@@ -10,13 +12,18 @@ const Content = (props) => {
                 <div className='author-info'>
                     <img src={authorImg} alt="" />
                     <div>
-                        <h5>{name}</h5>
+                        <h5 className='author-name'>{name}</h5>
                         <p>{date}</p>
                     </div>
                 </div>
-                <div className='reader-discription'>
+                <div className='read-discription'>
                     <p>{time} min read</p>
+                    <p><FontAwesomeIcon icon={faBookmark} /></p>
                 </div>
+            </div>
+            <div>
+                <h2>{title}</h2>
+                <a href="/read">Mark as read</a>
             </div>
         </div>
     );
